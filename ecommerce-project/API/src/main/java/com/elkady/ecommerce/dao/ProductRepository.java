17b308422fb750dd5026e62f1e11db23f0420550
ahmed <a.m.elkady@student.aast.edu>
2023-04-14 23:59:46 +0200

    add npm bootstrap and font awesome integrate template and support for java 19 and cross-origin

--- ecommerce-project/API/src/main/java/com/elkady/ecommerce/dao/ProductRepository.java
+++ ecommerce-project/API/src/main/java/com/elkady/ecommerce/dao/ProductRepository.java
@@ -1,9 +1,11 @@
 package com.elkady.ecommerce.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.elkady.ecommerce.entity.Product;
 
+@CrossOrigin
 public interface ProductRepository extends JpaRepository<Product,Long> {
     
 }
